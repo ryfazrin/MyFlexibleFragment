fe480f0121bedad151ddad28e5a468110b8777c5
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-22 16:54:19 +0800

    add DetailCategoryFragment and lateinit var
    add DetailCategoryFragment and lateinit variable

---
+++ app/src/main/java/com/ryfazrin/myflexiblefragment/DetailCategoryFragment.kt
@@ -0,0 +1,34 @@
+package com.ryfazrin.myflexiblefragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailCategoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DetailCategoryFragment : Fragment() {
+
+    lateinit var tvCategoryName: TextView
+    lateinit var tvCategoryDescription: TextView
+    lateinit var btnProfile: Button
+    lateinit var btnShowDialog: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
